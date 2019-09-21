266b65c40b5ae0667980769b2917d11ab14e7bea
m0nsecure <42235168+m0nsecure@users.noreply.github.com>
2019-09-21 03:08:42 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -43,6 +43,13 @@
 
 <body class="as-theme-light-heroimage">
     
+    <form action="/procesar-pago" method="POST">
+  <script
+   src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+   data-preference-id="<?php echo $preference->id; ?>">
+  </script>
+</form>
+    
     <?php
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
