a234e84643bf785bb6c52445f15b69b8c1306833
m0nsecure <42235168+m0nsecure@users.noreply.github.com>
2019-09-21 03:26:47 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -42,6 +42,28 @@
 
 
 <body class="as-theme-light-heroimage">
+    
+    
+    <?php
+// SDK de Mercado Pago
+require __DIR__ .  '/vendor/autoload.php';
+
+// Agrega credenciales
+MercadoPago\SDK::setAccessToken('APP_USR-1852356749457654-092104-5b92012a4245fda146694496ced7aae5-8481682');
+
+// Crea un objeto de preferencia
+$preference = new MercadoPago\Preference();
+
+// Crea un ítem en la preferencia
+$item = new MercadoPago\Item();
+$item->title = 'Mi producto';
+$item->quantity = 1;
+$item->unit_price = 75.56;
+$preference->items = array($item);
+$preference->save();
+?>
+    
+    
 
     <div class="stack">
         
