e15f8d5f6b3d8c8d04dfa586fda510d44785f43a
m0nsecure <42235168+m0nsecure@users.noreply.github.com>
2019-09-21 01:48:51 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -130,13 +130,7 @@
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <form action="/procesar-pago" method="POST">
-  <script
-   src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
-   data-preference-id="<?php echo $preference->id; ?>">
-  </script>
-</form>
-
+                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
                                 </div>
                             </div>
                         </div>
